f8d929d44f15ae13b8480b18d2a1cc0af364529a
WillmanGZ <willmang@uninorte.edu.co>
2025-07-14 13:46:16 -0500

    feat: login & register styles editted

--- src/auth/pages/login-page/login.js
+++ src/auth/pages/login-page/login.js
@@ -1,1 +1,16 @@
-export function loginSetup() {}
+import { redirectTo } from "../../../routes";
+
+export function loginSetup() {
+  //Take DOM references
+  const loginForm = document.getElementById("login-form");
+  const goRegister = document.getElementById("go-register");
+
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+  goRegister.addEventListener("click", (event) => {
+    event.preventDefault();
+    redirectTo("/register");
+  });
+}
--- src/auth/pages/register-page/register.js
+++ src/auth/pages/register-page/register.js
@@ -1,1 +1,16 @@
-export function registerSetup() {}
+import { redirectTo } from "../../../routes";
+
+export function registerSetup() {
+  //Take DOM references
+  const registerForm = document.getElementById("register-form");
+  const goLogin = document.getElementById("go-login");
+
+  registerForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
+
+  goLogin.addEventListener("click", (event) => {
+    event.preventDefault();
+    redirectTo("/login");
+  });
+}
