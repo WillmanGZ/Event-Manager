6372079fae27351b876c2d9ae809aa4a2e0b8b41
WillmanGZ <willmang@uninorte.edu.co>
2025-07-14 13:57:03 -0500

    enhance: improve styles

--- src/routes.js
+++ src/routes.js
@@ -7,33 +7,30 @@ import { notFoundSetup } from "./pages/not-found-page/not-found.js";
 const routes = {
   "/": {
     path: "/src/pages/landing-page/landing.html",
-    cssPath: "/src/pages/landing-page/landing.css",
+
     setup: landingSetup,
   },
   "/register": {
     path: "/src/auth/pages/register-page/register.html",
-    cssPath: "/src/auth/pages/register-page/register.css",
+
     setup: registerSetup,
   },
   "/login": {
     path: "/src/auth/pages/login-page/login.html",
-    cssPath: "/src/auth/pages/login-page/login.css",
+
     setup: loginSetup,
   },
   "/dashboard": {
     path: "/src/pages/dashboard-page/dashboard.html",
-    cssPath: "/src/pages/dashboard-page/dashboard.css",
+
     setup: dashboardSetup,
   },
   "/notFound": {
     path: "/src/pages/not-found-page/not-found.html",
-    cssPath: "/src/pages/not-found-page/not-found.css",
+
     setup: notFoundSetup,
   },
 };
-
-//Actual css styles
-let currentCssLink = null;
 
 export async function renderRoute() {
   const app = document.getElementById("main");
@@ -43,27 +40,6 @@ export async function renderRoute() {
   const route = routes[path] || routes["/notFound"];
 
   try {
-    //Delete old css before load new css
-    if (currentCssLink) {
-      currentCssLink.remove();
-    }
-
-    //Load css in document
-    if (route.cssPath) {
-      const newCssLink = document.createElement("link");
-      newCssLink.rel = "stylesheet";
-      newCssLink.href = route.cssPath;
-      document.head.appendChild(newCssLink);
-      currentCssLink = newCssLink;
-    }
-
-    //Wait untill css load
-    if (currentCssLink) {
-      await new Promise((resolve) => {
-        currentCssLink.onload = resolve;
-      });
-    }
-
     //Take html
     const file = await fetch(route.path);
     const content = await file.text();
